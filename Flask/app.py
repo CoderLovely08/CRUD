00122012f886b804365a97cb81895cfd8ee9d2ad
CoderLovely08 <lovelysingeras297@gmail.com>
2022-08-01 01:43:41 +0530

    Basic template ready

--- Flask/app.py
+++ Flask/app.py
@@ -3,4 +3,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    mylist=[
+        {"id":1,"name":"Lovely","age":20,"email":"lovelysingeras297@gmail.com"},
+        {"id":2,"name":"Om","age":21,"email":"om@gmail.com"},
+        {"id":3,"name":"Sahil","age":19,"email":"sahil@gmail.com"}
+    ]
+    return render_template('index.html',data=mylist)
+
+if __name__=="__main__":
+    app.run(debug=True)
